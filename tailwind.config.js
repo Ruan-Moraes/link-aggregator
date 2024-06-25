71e46196e86df4418b2d03841cb2451404af72de
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-06-25 19:39:39 -0300

    Finalizando projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './assets/js/*.mjs'],
   theme: {
     extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'serif'],
+      },
       minWidth: { screen: '100vw' },
       maxWidth: { '32rem': '32rem' },
       colors: {
         121212: '#121212',
-        '2d2e31': '#2d2e31',
         ff5f5a: '#ff5f5a',
         ff2c26: '#ff2c26',
       },
